f18885188ead4a1ed2f0b97410484d7b9cf8d223
Mrityunjay2302 <sonukkc5754@gmail.com>
2022-07-29 19:20:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+// import Switch  from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
+//import { createTheme } from '@material-ui/core/styles';
+
+function App() {
+  return (
+    
+    <>
+      <Header />
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Trending/>} exact />
+            <Route path="/movies" element={<Movies/>} />
+            <Route path="/series" element={<Series/>} />
+            <Route path="/search" element={<Search/>} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    
+    </>
+  );
+}
+
+export default App;
